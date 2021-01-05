f110401742100bbc06eb21a5cf8c62cdc5daea55
config <vichielman@gmail.com>
2021-01-05 09:38:04 +0100

    Update to system layout for downlink

---
+++ System Layout/Downlink Sizing/lens_sizing.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+
+
+#split focusing into two parts-> vacuum and atmoshpere part
+#diffraction formula
+#difflim  = lambd/(2*n*np.sin(theta))
+#difflim is max radius on earth
+#lambd is wavelength 
+#n is refractive index medium
+#theta is angle >
+
+
+#divergence sunlight
+x = 1
