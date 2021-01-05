5ea96a8e5f1b24520301dbf9f74c2931980ad82b
config <vichielman@gmail.com>
2021-01-05 10:11:17 +0100

    update to workerparabola

--- System Layout/workerparabola.py
+++ System Layout/workerparabola.py
@@ -10,16 +10,17 @@ n_ground = 0.27
 n_atmosphere = 0.55
 n_rigid = 0.98
 n_foil = 0.95
+n_lens
 flux_solar = 1361 #W/m2
 
 p_intake = p_ground/(n_ground*n_atmosphere*(n_rigid**2)*(n_foil**2))
 
 s_intake = p_intake/flux_solar
-r_intake = np.sqrt(s_intake)/np.pi
+r_intake = np.sqrt(s_intake/np.pi)
 
 #thermal power @workerparabola
 p_therm_worker = (p_intake*n_foil)*(1-n_foil) #worker parabola assumed to be foil for now, could be changed to rigid
 
 s_rad =  p_therm_worker/p_diss
-r_rad = np.sqrt(s_rad)/np.pi
+r_rad = np.sqrt(s_rad/np.pi)
 print(r_rad)
