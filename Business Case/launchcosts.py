038acce350a987902d98a428112e69c908fdb20b
MvDalGit <vichielman@gmail.com>
2021-01-06 11:42:56 +0100

    the cost of greatness

---
+++ Business Case/launchcosts.py
@@ -0,0 +1,3 @@
+#the launch costs should solely be determined by the amount of launches required-> we deliver a hypothetical package to spacex that they insert into orbit.
+#The only thing that is required is the # of launches
+#multiplied by the launch cost
---
+++ Business Case/totalcosts.py
@@ -0,0 +1,15 @@
+#total costs will be composed of the following 
+#(split into hard costs and soft costs)
+#HARD COSTS-> the costs that are calculated by fixed prices, not estimated
+#-material/component costs 
+#-launch costs
+#-ground station costs
+#-maintenance costs (component replacement cost + cost of launching component)
+#SOFT COSTS-> these costs are estimated based upon locations of manufacturer
+#wages, transportation distance etc.
+#-assembly costs (on ground)
+#-transport costs (transporting from manufacturing/assembly locations to launch sites)
+#-maintenance costs (transportation and assembly of maintenance components)
+#-operation costs (ground station personnel, launch and space assembly personnel, design personnel etc.)
+
+
